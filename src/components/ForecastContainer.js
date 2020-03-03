0120a98dc96b3b3de3dde9b299f8212ac55d6f8c
neeka <neekaishappy@gmail.com>
2020-03-03 15:28:58 -0800

    fix error handling

--- src/components/ForecastContainer.js
+++ src/components/ForecastContainer.js
@@ -11,18 +11,16 @@ import './ForecastContainer.css';
 const NUM_DAILY_FORECAST = 5;
 
 function ForecastContainer() {
-  const [location, setLocation] = useState();
-  // {
-  //   city: 'Austin',
-  //   coords: [30.2672, -97.7431]
-  // }
+  const [location, setLocation] = useState(false);
   const [currentForecast, setCurrentForecast] = useState({});
   const [dailyForecast, setDailyForecast] = useState([]);
   const [timezone, setTimezone] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
 
   async function getWeatherData() {
+    setIsLoading(true);
+    setErrorMessage(false);
     try {
       const URL = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${location.coords[0]},${location.coords[1]}`;
       const res = await axios.get(URL);
@@ -45,20 +43,15 @@ function ForecastContainer() {
       }
       setDailyForecast(forecastWeek);
       setTimezone(data.timezone);
-      setErrorMessage(null);
-      setIsLoading(false);
     } catch (err) {
-      debugger;
-      console.log(err);
-      // setErrorMessage('Something went wrong. Please try again later');
+      setErrorMessage('Something went wrong. Please try again later Dark Sky');
     }
-    // finally {
-    //   setIsLoading(false);
-    // }
+    setIsLoading(false);
   }
 
   function getGeolocation() {
     function geoSuccess(position) {
+      setIsLoading(true);
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
 
@@ -69,23 +62,21 @@ function ForecastContainer() {
           const { data } = res;
           setLocation({ city: data.features[0].text, coords: [lat, long] });
         } catch (err) {
-          // setErrorMessage('Something went wrong. Please try again later');
+          setErrorMessage(
+            'Something went wrong. Please try again later Mapbox'
+          );
         }
-        // finally {
-        //   setIsLoading(false);
-        // }
+        setIsLoading(false);
       }
       getCityFromCoords(latitude, longitude);
     }
     function geoError() {
       setErrorMessage('Please allow your location or enter a city above.');
-      setIsLoading(false);
     }
     if (!navigator.geolocation) {
       setErrorMessage(
         'Geolocation is not supported by your browser. Please enter a city above.'
       );
-      setIsLoading(false);
     } else {
       navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
     }
@@ -99,16 +90,17 @@ function ForecastContainer() {
   }, [location]);
 
   function updateLocation(newCity) {
-    setIsLoading(true);
     setLocation(newCity);
   }
 
   let result;
-  if (errorMessage && !isLoading) {
+  if (errorMessage) {
     result = <ErrorMessage errorMessage={errorMessage} />;
-  } else if (isLoading) {
+  }
+  if (isLoading) {
     result = <Loader />;
-  } else {
+  }
+  if (!errorMessage && !isLoading) {
     result = (
       <>
         <CityHeader city={location.city} />
@@ -117,12 +109,15 @@ function ForecastContainer() {
       </>
     );
   }
-
   return (
     <div className="ForecastContainer">
       <CitySearchForm updateLocation={updateLocation} />
       <hr />
-      {result}
+      {location ? (
+        result
+      ) : (
+        <h2>Please allow your location or enter a city above</h2>
+      )}
     </div>
   );
 }
